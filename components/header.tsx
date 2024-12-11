b836402a5b8f56e5cf4cd7ce0fb082ea92bf8485
kaiyang0815 <wangkaiyang0815@gmail.com>
2024-12-11 08:01:32 +0800

    Add: 新增动画效果

--- components/header.tsx
+++ components/header.tsx
@@ -1,33 +1,65 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { motion } from "motion/react";
 import Link from "next/link";
-import RandomSentence from "./random-sentence";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const path = usePathname();
+
   return (
     <header className="flex flex-col items-start gap-4 pb-4 pt-16">
-      <h1 className="text-4xl font-black">凯阳的备忘录</h1>
-      <div className="text-neutral-5 text-sm">
-        <RandomSentence />
-      </div>
-      <nav className="flex gap-6 text-lg">
-        <Link
-          href="/"
-          className="text-neutral-500 transition-colors hover:text-neutral-900"
-        >
-          首页
-        </Link>
-        <Link
-          href="/projects"
-          className="text-neutral-500 transition-colors hover:text-neutral-900"
-        >
-          项目
-        </Link>
-        <Link
-          href="/about"
-          className="text-neutral-500 transition-colors hover:text-neutral-900"
-        >
-          关于
-        </Link>
-      </nav>
+      <motion.div
+        initial={{ opacity: 0, y: -100 }}
+        animate={{
+          opacity: 1,
+          y: 0,
+          transition: { duration: 0.5 },
+        }}
+        viewport={{ once: true }}
+      >
+        <h1 className="text-4xl font-black">凯阳的备忘录</h1>
+      </motion.div>
+
+      <div className="text-neutral-5 text-sm">{/* <RandomSentence /> */}</div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: { duration: 2 },
+        }}
+      >
+        <nav className="flex gap-6 text-lg">
+          <Link
+            href="/"
+            className={cn(
+              path == "/" ? "text-neutral-900" : "text-neutral-500",
+              "transition-colors hover:text-neutral-900",
+            )}
+          >
+            首页
+          </Link>
+          {/* <Link
+            href="/projects"
+            className={cn(
+              path == "/projects" ? "text-neutral-900" : "text-neutral-500",
+              "transition-colors hover:text-neutral-900",
+            )}
+          >
+            项目
+          </Link> */}
+          <Link
+            href="/about"
+            className={cn(
+              path == "/about" ? "text-neutral-900" : "text-neutral-500",
+              "transition-colors hover:text-neutral-900",
+            )}
+          >
+            关于
+          </Link>
+        </nav>
+      </motion.div>
       <hr />
     </header>
   );
