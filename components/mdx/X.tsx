d3815518fbddfa88d1802ca0ba7c80071188d000
kaiyang0815 <wangkaiyang0815@gmail.com>
2024-12-12 09:38:51 +0800

    Fix: 修复 lint 错误

--- components/mdx/X.tsx
+++ components/mdx/X.tsx
@@ -3,8 +3,8 @@ import type { TwitterComponents } from "react-tweet";
 import { Tweet } from "react-tweet";
 
 const components: TwitterComponents = {
-  AvatarImg: (props) => <Image {...props} />,
-  MediaImg: (props) => <Image {...props} fill unoptimized />,
+  AvatarImg: (props) => <Image {...props} alt="AvatarImg" />,
+  MediaImg: (props) => <Image {...props} fill unoptimized alt="MediaImg" />,
 };
 
 export default function X({ id }: { id: string }) {
